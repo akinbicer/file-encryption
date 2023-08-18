74ec27242eb8b052193bf8fc3c3735bad5cad8bd
Akın BİÇER <akin.bicer@outlook.com.tr>
2023-08-18 11:05:36 +0300

    Added model summaries

--- FileEncryption/Models/SecurityKey.cs
+++ FileEncryption/Models/SecurityKey.cs
@@ -1,9 +1,27 @@
 ﻿namespace FileEncryption.Models;
 
+/// <summary>
+///     Represents a pair of cryptographic keys, including a public key and a private key.
+/// </summary>
 public class SecurityKey
 {
+    /// <summary>
+    ///     Gets or sets the unique identifier of the security key.
+    /// </summary>
     public Guid Id { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the creation date of the security key.
+    /// </summary>
     public DateTime CreatedDate { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the public key.
+    /// </summary>
     public required string PublicKey { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the private key.
+    /// </summary>
     public required string PrivateKey { get; set; }
 }
